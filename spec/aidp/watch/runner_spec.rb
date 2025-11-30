34dd2d420e531924ad4fbbb9fd9ff5e863d28e53
Bart Agapinan <bart@sonic.net>
2025-11-30 18:32:09 +0000

    test: Standardize hash syntax and improve formatting in test_prompt and runner specs

--- spec/aidp/watch/runner_spec.rb
+++ spec/aidp/watch/runner_spec.rb
@@ -17,9 +17,12 @@ RSpec.describe Aidp::Watch::Runner do
   let(:auto_update_policy) { instance_double("AutoUpdatePolicy", enabled: true, check_interval_seconds: 1) }
   let(:auto_update_check) { instance_double("UpdateCheck", should_update?: true, current_version: "1", available_version: "2") }
   let(:auto_update) { instance_double("AutoUpdateCoordinator", check_for_updates: nil, check_for_update: auto_update_check, policy: auto_update_policy, restore_from_checkpoint: nil, hot_reload_available?: false, initiate_update: nil) }
+  let(:issue_detail) { {number: 1, labels: ["plan"], comments: [], author: "alice"} }
 
   before do
-    stub_const("Aidp::Watch::GitHubStateExtractor", Class.new)
+    extractor_class = Class.new
+    extractor_class.const_set(:IN_PROGRESS_LABEL, "aidp-in-progress")
+    stub_const("Aidp::Watch::GitHubStateExtractor", extractor_class)
     allow(Aidp::Watch::RepositoryClient).to receive(:parse_issues_url).and_return(["o", "r"])
     allow(Aidp::Watch::RepositoryClient).to receive(:new).and_return(repo_client)
     allow(Aidp::Watch::RepositorySafetyChecker).to receive(:new).and_return(safety_checker)
@@ -36,6 +39,15 @@ RSpec.describe Aidp::Watch::Runner do
     allow_any_instance_of(described_class).to receive(:display_message)
     allow(Aidp).to receive(:log_info)
     allow(Aidp).to receive(:log_debug)
+    allow(Aidp).to receive(:log_error)
+    allow(Aidp).to receive(:log_warn)
+
+    allow(state_extractor).to receive_messages(
+      in_progress?: false,
+      detection_comment_posted?: false,
+      build_completed?: false
+    )
+    allow(safety_checker).to receive(:should_process_issue?).and_return(true)
   end
 
   describe "#start" do
@@ -84,6 +96,93 @@ RSpec.describe Aidp::Watch::Runner do
     it "enables post_detection_comments by default" do
       runner = described_class.new(issues_url: "o/r", once: true)
       expect(runner.instance_variable_get(:@post_detection_comments)).to be true
+    end
+  end
+
+  describe "trigger processing" do
+    let(:runner) { described_class.new(issues_url: "o/r", once: true) }
+
+    before do
+      allow(runner).to receive(:post_detection_comment)
+    end
+
+    it "processes plan triggers and posts detection comment" do
+      allow(repo_client).to receive(:list_issues).and_return([{number: 1, labels: ["plan"]}])
+      allow(repo_client).to receive(:fetch_issue).and_return(issue_detail)
+
+      runner.send(:process_plan_triggers)
+
+      expect(plan_processor).to have_received(:process).with(issue_detail)
+      expect(runner).to have_received(:post_detection_comment)
+    end
+
+    it "skips when plan poll fails" do
+      allow(repo_client).to receive(:list_issues).and_raise(StandardError.new("boom"))
+
+      expect { runner.send(:process_plan_triggers) }.not_to raise_error
+    end
+
+    it "processes build triggers with in-progress label handling" do
+      allow(repo_client).to receive(:list_issues).and_return([{number: 2, labels: ["build"]}])
+      allow(repo_client).to receive(:fetch_issue).and_return(issue_detail.merge(number: 2, labels: ["build"]))
+      allow(repo_client).to receive(:add_labels)
+      allow(repo_client).to receive(:remove_labels)
+
+      runner.send(:process_build_triggers)
+
+      expect(build_processor).to have_received(:process)
+      expect(repo_client).to have_received(:add_labels)
+      expect(repo_client).to have_received(:remove_labels)
+    end
+
+    it "processes auto issue triggers" do
+      allow(auto_processor).to receive(:auto_label).and_return("auto")
+      allow(repo_client).to receive(:list_issues).and_return([{number: 3, labels: ["auto"]}])
+      allow(repo_client).to receive(:fetch_issue).and_return(issue_detail.merge(number: 3, labels: ["auto"]))
+
+      runner.send(:process_auto_issue_triggers)
+
+      expect(auto_processor).to have_received(:process)
+    end
+
+    it "processes review triggers" do
+      allow(review_processor).to receive(:review_label).and_return("review")
+      allow(repo_client).to receive(:list_pull_requests).and_return([{number: 4, labels: ["review"]}])
+      allow(repo_client).to receive(:fetch_pull_request).and_return({number: 4, labels: ["review"]})
+
+      runner.send(:process_review_triggers)
+
+      expect(review_processor).to have_received(:process)
+    end
+
+    it "processes auto PR triggers" do
+      allow(auto_pr_processor).to receive(:auto_label).and_return("auto-pr")
+      allow(repo_client).to receive(:list_pull_requests).and_return([{number: 5, labels: ["auto-pr"]}])
+      allow(repo_client).to receive(:fetch_pull_request).and_return({number: 5, labels: ["auto-pr"]})
+
+      runner.send(:process_auto_pr_triggers)
+
+      expect(auto_pr_processor).to have_received(:process)
+    end
+
+    it "processes ci fix triggers" do
+      allow(ci_fix_processor).to receive(:ci_fix_label).and_return("ci-fix")
+      allow(repo_client).to receive(:list_pull_requests).and_return([{number: 6, labels: ["ci-fix"]}])
+      allow(repo_client).to receive(:fetch_pull_request).and_return({number: 6, labels: ["ci-fix"]})
+
+      runner.send(:process_ci_fix_triggers)
+
+      expect(ci_fix_processor).to have_received(:process)
+    end
+
+    it "processes change request triggers" do
+      allow(change_request_processor).to receive(:change_request_label).and_return("cr")
+      allow(repo_client).to receive(:list_pull_requests).and_return([{number: 7, labels: ["cr"]}])
+      allow(repo_client).to receive(:fetch_pull_request).and_return({number: 7, labels: ["cr"]})
+
+      runner.send(:process_change_request_triggers)
+
+      expect(change_request_processor).to have_received(:process)
     end
   end
 end
